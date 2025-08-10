df22c7d1f2948f3837f39d9b39203c7041f126c9
SoumyajitSaha1999 <129038520+SoumyajitSaha1999@users.noreply.github.com>
2025-08-10 22:55:48 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,6 @@
-amazon-linux-extras install ansible2 -y
-yum install python3 python-pip python-dlevel -y
+sudo dnf install ansible -y
+sudo dnf install python3 python3-pip -y
+ansible --version
+
+# amazon-linux-extras install ansible2 -y
+# yum install python3 python-pip python-dlevel -y
